54609253e27900104bc3f8a321824a215f824d27
Adarsh Nair <adarsh@Adarshs-Air.lan>
2024-02-03 21:35:11 -0500

    fetching data from google sheets and displaying running balance as test

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import './App.css';
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [transactions, setTransactions] = useState(null)
+
+  const ENDPOINT = process.env.REACT_APP_SCRIPT_ENDPOINT;
+
+  useEffect(() => {
+    fetch(`${ENDPOINT}`)
+      .then(res => res.json())
+      .then(data => {
+        setTransactions(data)
+        setLoading(false)
+      })
+  }, []);
+
+  const getRunningBalance = () => {
+    let balance = transactions[transactions.length - 1]["Running Balance"].toFixed(2);
+    return toNumberWithCommas(balance);
+  }
+
+  const toNumberWithCommas = (n) => {
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   return (
     <div className="App">
-      <p>Hello, world</p>
+      {loading ? <p>Loading...</p> : <p>Running Balance: ${getRunningBalance()} </p>}
     </div>
   );
 }
