bdf908f5ed79fb5761718b6dd7921aabef0da8f1
Adarsh Nair <adarsh@Adarshs-Air.lan>
2024-03-02 19:25:22 -0500

    moved env into server directory

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 const app = express();
 
